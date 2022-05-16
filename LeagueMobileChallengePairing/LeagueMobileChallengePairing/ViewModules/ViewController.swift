4596bcdc95beff60b381af19140e0abc584c6a69
danielbs2 <daniel.passos@bs2tecnologia.com.br>
2022-05-16 10:52:54 -0300

    add initial files

---
+++ LeagueMobileChallengePairing/LeagueMobileChallengePairing/ViewModules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeagueMobileChallengePairing
+//
+//  Created by Daniel Maia dos Passos on 16/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  
+
+
+}
+
